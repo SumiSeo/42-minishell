2fe96298852f28ec29ea29960869b3080d96dd98
Sumi Seo <sumseo@bess-f1r5s3.clusters.42paris.fr>
2024-05-18 14:18:28 +0200

    feat/modify-header

--- minishell.h
+++ minishell.h
@@ -6,14 +6,21 @@
 /*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 13:49:55 by sumseo            #+#    #+#             */
-/*   Updated: 2024/05/18 14:15:11 by sumseo           ###   ########.fr       */
+/*   Updated: 2024/05/18 14:18:18 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <errno.h>
+# include <fcntl.h>
+# include <signal.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <term.h>
 # include <unistd.h>
 
 #endif
