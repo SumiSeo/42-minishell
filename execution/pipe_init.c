db9b130a131631b04bd9c7ce2fd41f7482bdf73d
Sumi Seo <sumseo@bess-f1r4s4.clusters.42paris.fr>
2024-06-08 17:22:18 +0200

    feat/built-in-finish

--- execution/pipe_init.c
+++ execution/pipe_init.c
@@ -6,7 +6,7 @@
 /*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 17:59:43 by sumseo            #+#    #+#             */
-/*   Updated: 2024/06/07 23:01:12 by sumseo           ###   ########.fr       */
+/*   Updated: 2024/06/08 16:56:58 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ void	create_pipe(t_parse *cmds_list, char **env_copy, t_data *data,
 {
 	int	fork_id;
 
+	printf("Check create pipe function\n");
 	(void)cmds_list;
 	(void)env_copy;
 	(void)data;
