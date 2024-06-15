be734162445125f99d2165a08f78278d0ed27152
Sumi Seo <sumseo@bess-f1r5s3.clusters.42paris.fr>
2024-06-15 16:45:01 +0200

    feat/finish-multi-pipe

--- execution/pipe_init.c
+++ execution/pipe_init.c
@@ -6,15 +6,14 @@
 /*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 17:59:43 by sumseo            #+#    #+#             */
-/*   Updated: 2024/06/15 16:01:10 by sumseo           ###   ########.fr       */
+/*   Updated: 2024/06/15 16:34:58 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	getfile(t_parse *cmds_list, t_pipe *pipe_info)
+void	getfile(t_parse *cmds_list)
 {
-	(void)pipe_info;
 	if (cmds_list == NULL)
 	{
 		fprintf(stderr, "cmds_list is NULL\n");
@@ -28,31 +27,16 @@ void	getfile(t_parse *cmds_list, t_pipe *pipe_info)
 	else if (cmds_list->infile_token && ft_strncmp(cmds_list->infile_token, "<",
 			1) == 0)
 	{
-		printf("normal infile\n");
 		cmds_list->infile = open(cmds_list->infile_name, O_RDONLY);
-	}
-	else
-	{
-		printf("STANDARD INPUT\n");
 	}
 	if (cmds_list->outfile_token && ft_strncmp(cmds_list->outfile_token, ">>",
 			2) == 0)
-	{
-		printf("append outfile\n");
 		cmds_list->outfile = open(cmds_list->outfile_name, O_RDWR | O_APPEND,
 				0644);
-	}
 	else if (cmds_list->outfile_token && ft_strncmp(cmds_list->outfile_token,
 			">", 1) == 0)
-	{
-		printf("normal outfile\n");
 		cmds_list->outfile = open(cmds_list->outfile_name,
 				O_WRONLY | O_TRUNC | O_CREAT, 0644);
-	}
-	else if (!cmds_list->outfile_token)
-	{
-		printf("STANDARD OUTPUT\n");
-	}
 }
 void	close_pipe_files(t_parse *cmds_list, t_pipe *pipe_info)
 {
@@ -70,6 +54,7 @@ void	close_pipe_files(t_parse *cmds_list, t_pipe *pipe_info)
 		}
 		if (cmds_list)
 			cmds_list = cmds_list->next;
+		wait(0);
 		i++;
 	}
 }
@@ -78,9 +63,6 @@ void	pipe_init(t_pipe *pipe_info, t_parse *cmds_list, int i, t_data *data)
 {
 	int	fd[2];
 
-	(void)i;
-	(void)pipe_info;
-	(void)cmds_list;
 	if (data->has_pipe < 1)
 		return ;
 	else
@@ -98,47 +80,9 @@ void	pipe_init(t_pipe *pipe_info, t_parse *cmds_list, int i, t_data *data)
 	}
 }
 
-void	execute_pipeline(t_parse *cmds_list, char **env_copy, t_data *data)
-{
-	t_pipe	*pipe_info;
-	int		i;
-	int		fork_id;
-
-	// int		status;
-	pipe_info = malloc(sizeof(t_pipe));
-	if (pipe_info == NULL)
-	{
-		perror("pipe_info_malloc");
-		exit(EXIT_FAILURE);
-	}
-	i = 0;
-	pipe_info->total_cmds = count_cmds(cmds_list);
-	while (i < pipe_info->total_cmds)
-	{
-		pipe_init(pipe_info, cmds_list, i, data);
-		getfile(cmds_list, pipe_info);
-		fork_id = fork();
-		if (fork_id == -1)
-			perror("fork");
-		if (fork_id == 0)
-		{
-			redirection(cmds_list, pipe_info, env_copy, i);
-			parse_path(cmds_list, env_copy);
-		}
-		i++;
-		if (i == pipe_info->total_cmds)
-			break ;
-		cmds_list = cmds_list->next;
-	}
-	close_pipe_files(cmds_list, pipe_info);
-	wait(0);
-	wait(0);
-	free(pipe_info);
-}
-
 void	free_fork_pids(t_pipe *pipe_info)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < pipe_info->total_cmds)
@@ -151,4 +95,39 @@ void	free_fork_pids(t_pipe *pipe_info)
 		i++;
 	}
 	free(pipe_info->pids);
+}
+
+void	execute_pipeline(t_parse *cmds_list, char **env_copy, t_data *data)
+{
+	t_pipe	*pipe_info;
+	int		i;
+	int		fork_id;
+
+	pipe_info = malloc(sizeof(t_pipe));
+	if (pipe_info == NULL)
+	{
+		perror("pipe_info_malloc");
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	pipe_info->total_cmds = count_cmds(cmds_list);
+	while (i < pipe_info->total_cmds)
+	{
+		pipe_init(pipe_info, cmds_list, i, data);
+		getfile(cmds_list);
+		fork_id = fork();
+		if (fork_id == -1)
+			perror("fork");
+		if (fork_id == 0)
+		{
+			redirection(cmds_list, pipe_info, i);
+			parse_path(cmds_list, env_copy);
+		}
+		i++;
+		if (i == pipe_info->total_cmds)
+			break ;
+		cmds_list = cmds_list->next;
+	}
+	close_pipe_files(cmds_list, pipe_info);
+	free(pipe_info);
 }
--- execution/redirect_init.c
+++ execution/redirect_init.c
@@ -6,7 +6,7 @@
 /*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 16:13:20 by sumseo            #+#    #+#             */
-/*   Updated: 2024/06/15 16:04:19 by sumseo           ###   ########.fr       */
+/*   Updated: 2024/06/15 16:42:20 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,10 +47,8 @@ void	only_redirection(t_parse *cmds_list)
 	close(cmds_list->outfile);
 }
 
-void	redirection(t_parse *cmds_list, t_pipe *pipe_info, char **env_copy,
-		int i)
+void	redirection(t_parse *cmds_list, t_pipe *pipe_info, int i)
 {
-	(void)env_copy;
 	(void)cmds_list;
 	(void)i;
 	if (!cmds_list || !pipe_info)
@@ -82,9 +80,6 @@ void	redirection(t_parse *cmds_list, t_pipe *pipe_info, char **env_copy,
 	}
 	else if (i == pipe_info->total_cmds - 1)
 	{
-		printf("Last command\n");
-		printf("cmds_list->prev->pipe_fdi %d\n", cmds_list->prev->pipe_fdi);
-		printf("cmds_list->prev->pipe_fdo %d\n", cmds_list->prev->pipe_fdo);
 		if (cmds_list->outfile_token)
 		{
 			dup2(cmds_list->outfile, STDOUT_FILENO);
@@ -106,23 +101,21 @@ void	redirection(t_parse *cmds_list, t_pipe *pipe_info, char **env_copy,
 	}
 	else
 	{
-		// middle command
 		if (cmds_list->infile_name)
 		{
 			dup2(cmds_list->infile, STDIN_FILENO);
 			close(cmds_list->infile);
 			close(cmds_list->prev->pipe_fdi);
 		}
-		if (cmds_list->outfile_name)
+		if (cmds_list->outfile_token)
 		{
 			dup2(cmds_list->outfile, STDOUT_FILENO);
 			close(cmds_list->outfile);
 			close(cmds_list->prev->pipe_fdo);
 		}
-		else
-		{
-			dup2(cmds_list->prev->pipe_fdi, STDIN_FILENO);
-			dup2(cmds_list->prev->pipe_fdo, STDOUT_FILENO);
-		}
+		dup2(cmds_list->pipe_fdi, STDIN_FILENO);
+		close(cmds_list->prev->pipe_fdi);
+		dup2(cmds_list->pipe_fdo, STDOUT_FILENO);
+		close(cmds_list->prev->pipe_fdo);
 	}
 }
