25ad71809edce4b0dcc7cb4df161b59fec65b0f2
Sumi Seo <sumseo@bess-f1r3s1.clusters.42paris.fr>
2024-06-21 15:05:39 +0200

    fix/noraml-exit

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 19:11:30 by sumseo            #+#    #+#             */
-/*   Updated: 2024/06/21 15:01:52 by sumseo           ###   ########.fr       */
+/*   Updated: 2024/06/21 15:05:29 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,23 @@ int	is_exit(char *str)
 	return (0);
 }
 
+void	control_alpha(char *s)
+{
+	printf("exit : %s: numeric argument required\n", s);
+	exit(1);
+}
+
+void	control_many_args(void)
+{
+	printf("exit: too many arugments\n");
+	exit(1);
+}
+
+void	normal_exit(void)
+{
+	printf("exit\n");
+	exit(1);
+}
 void	func_exit(t_parse *cmds)
 {
 	int	i;
@@ -39,18 +56,12 @@ void	func_exit(t_parse *cmds)
 	while (cmds->cmd_array[1][i])
 	{
 		if (ft_isalpha(cmds->cmd_array[1][i]))
-		{
-			printf("exit : %s: numeric argument required\n",
-				cmds->cmd_array[1]);
-			exit(1);
-			break ;
-		}
+			control_alpha(cmds->cmd_array[1]);
 		i++;
 	}
 	if (cmds->cmd_array[1] && cmds->cmd_array[2])
 	{
-		printf("exit: too many arugments\n");
-		exit(1);
+		control_many_args();
 		return ;
 	}
 	i = 0;
@@ -58,18 +69,9 @@ void	func_exit(t_parse *cmds)
 	{
 		printf("%s\n", cmds->cmd_array[i]);
 		if (!ft_isdigit(cmds->cmd_array[1][i]))
-		{
-			printf("exit : %s: numeric argument required\n",
-				cmds->cmd_array[1]);
-			exit(1);
-			break ;
-		}
+			control_alpha(cmds->cmd_array[1]);
 		else
-		{
-			exit(1);
-			printf("exit\n");
-			return ;
-		}
+			normal_exit();
 		i++;
 	}
 }
